110b2460820297357f32ccd4b5a04eb0f950f62a
rahman <rahman821.ar@gmail.com>
2024-12-25 21:26:59 +0530

    list selection added

--- ReactApp/src/components/ListGroup.tsx
+++ ReactApp/src/components/ListGroup.tsx
@@ -1,15 +1,44 @@
+// import { MouseEvent } from "react";
 
+
+import { useState } from "react";
 
 function ListGroup(){
-    const items = ["New York", "San Francisco", "Tokyo", "London"];
+    let items = ["New York", "San Francisco", "Tokyo", "London"];
 
+    // event handling
+    // let selected_index = -1;
+    // hook
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    // const handleClick = (event: MouseEvent) => console.log(event)
+    // items = []
+    // Conditional rendering 
+    // this method creates duplication
+    // if (items.length ===0 ){
+    //     return <>
+    //     <h1>List</h1>
+    //     <p>No item found</p>;
+    //     </> 
+    // }
+    const getMessage = () => {
+        return items.length===0 ? <p>No item found</p> : null;
+    }
     // Fragment is a useful feature in React to group multiple elements together without adding extra nodes to the DOM.
     return (
         <>
             <h1>List</h1>
+            { items.length === 0 ? <p>{getMessage()}</p> : null}
+            {/* { items.length===0 && <p>No item found</p>} */}
             <ul className="list-group">
-                {items.map((item) => (
-                    <li key={item}>{item}</li>    
+                {items.map((item, index) => (
+                    <li 
+                        className={ selectedIndex === index?"list-group-item active":"list-group-item"} 
+                        key={item} 
+                        onClick={()=>{setSelectedIndex(index)}}
+                    >
+                        {item}
+                    </li>    
             ))}
             </ul>
         </>
