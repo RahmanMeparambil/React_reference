d7c8bb2f03bbe0677f770169985ce60ca6c30391
rahman <rahman821.ar@gmail.com>
2024-12-26 12:16:00 +0530

    passing children to the component

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,8 +1,13 @@
-import ListGroup from './components/ListGroup';
+// import ListGroup from './components/ListGroup';
 // import Message from './Message'
+import Alert from "./components/Alert";
 
 function App(){
-  return <div><ListGroup items={["New York", "San Francisco", "Tokyo", "London"]} heading='Cities' /></div>;
+  return (<div>
+    <Alert>
+      Hello_<span>World</span>
+    </Alert>
+        </div>)
 }
 
 export default App;
